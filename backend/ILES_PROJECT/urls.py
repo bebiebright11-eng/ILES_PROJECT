f91bef75f458c7f3dbe6f40d2a0e7933691231f7
bebiebright11-eng <bebiebright11@gmail.com>
2026-04-03 14:03:58 +0300

    Another project

--- backend/ILES_PROJECT/urls.py
+++ backend/ILES_PROJECT/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+admin.site.site_header = "Internship Placement System"
+admin.site.site_title = "Internship Admin"
+admin.site.index_title = "Welcome to Internship Dashboard"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/accounts/', include('accounts.urls')),
+    path('api/internships/', include('internships.urls')),
+    path('api/supervision/', include('supervision.urls')),
 ]
---
+++ backend/accounts/urls.py
@@ -0,0 +1,13 @@
+# accounts/urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet
+
+# Create a router and register the viewsets
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
+
+# The API URLs are now determined automatically by the router
+urlpatterns = [
+    path('', include(router.urls)),
+]
